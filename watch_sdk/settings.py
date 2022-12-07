bbaa7b7ce595bd306d06b080c8003c5896cb45c5
Pulkit Goyal <7895pulkit@gmail.com>
2022-12-07 14:57:48 +0530

    allow all hosts

--- watch_sdk/settings.py
+++ watch_sdk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-43p1j$+3268$upf$4^+q=4#zto-v^6=n)(qh7cbxlo5a+*aisl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
