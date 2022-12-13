76e5d137b10b98e12c336e1ad31d783e936d4438
Pulkit Goyal <7895pulkit@gmail.com>
2022-12-13 19:08:31 +0530

    remove unused imports

--- watch_sdk/utils.py
+++ watch_sdk/utils.py
@@ -1,13 +1,6 @@
 import json
 from watch_sdk.models import UserApp, WatchConnection
 import requests
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 
 def google_fit_cron():
