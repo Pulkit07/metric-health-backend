06f2a303566719acc25eb989c238ce5f7d3ecf39
Pulkit Goyal <7895pulkit@gmail.com>
2023-02-02 16:55:07 +0530

    make enabled platforms nullable

--- watch_sdk/models.py
+++ watch_sdk/models.py
@@ -33,7 +33,7 @@ class UserApp(BaseModel):
     website = models.CharField(max_length=100, blank=True, null=True)
     webhook_url = models.CharField(max_length=600, blank=True, null=True)
     key = models.CharField(max_length=100, blank=True, null=True)
-    enabled_platforms = models.ManyToManyField(EnabledPlatform, blank=True)
+    enabled_platforms = models.ManyToManyField(EnabledPlatform, blank=True, null=True)
     google_auth_client_id = models.CharField(max_length=200, blank=True, null=True)
     payment_plan = models.CharField(
         max_length=100,
