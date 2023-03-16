c4d299569101569f382958deee30a1bea8bc1829
Pulkit Goyal <7895pulkit@gmail.com>
2023-03-16 22:22:55 +0530

    send mail on new user creation too

--- watch_sdk/mail_utils.py
+++ watch_sdk/mail_utils.py
@@ -1,10 +1,20 @@
 import os
 from azure.communication.email import EmailClient
 from celery import shared_task
-from watch_sdk.models import UserApp
+from watch_sdk.models import User, UserApp
 
 connection_string = os.getenv("AZURE_COMMUNICATION_SERVICES_CONNECTION_STRING")
 client = EmailClient.from_connection_string(connection_string)
+
+
+@shared_task
+def send_email_on_new_user(user_id):
+    user = User.objects.get(id=user_id)
+    send_email.delay(
+        to=os.getenv("ADMIN_EMAIL_ADDRESS"),
+        subject="[HEKA BACKEND] New user has joined the platform",
+        body=f"New user has been created: {user.email} {user.email} {user.company_name} {user.country}",
+    )
 
 
 @shared_task
@@ -12,8 +22,8 @@ def send_email_on_new_app(user_app_id):
     app = UserApp.objects.get(id=user_app_id)
     send_email.delay(
         to=os.getenv("ADMIN_EMAIL_ADDRESS"),
-        subject="New app",
-        body=f"New app has been created by {app.user.email}",
+        subject="[HEKA BACKEND] New app has been created",
+        body=f"New app {app.name} has been created by {app.user.email}",
     )
 
 
--- watch_sdk/signals.py
+++ watch_sdk/signals.py
@@ -7,6 +7,13 @@ from watch_sdk.data_providers.fitbit import FitbitAPIClient
 from .models import EnabledPlatform, Platform
 
 
+@receiver(post_save, sender="watch_sdk.User")
+def mail_on_new_user(sender, instance, created, **kwargs):
+    if not created:
+        return
+    mail_utils.send_email_on_new_user.delay(instance.id)
+
+
 @receiver(post_save, sender="watch_sdk.UserApp")
 def enable_basic_platforms(sender, instance, created, **kwargs):
     if not created:
