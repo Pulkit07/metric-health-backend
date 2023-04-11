0e381f1d05eb1ca440a2d597e30d27ce508295f4
Pulkit Goyal <7895pulkit@gmail.com>
2023-04-11 17:31:57 +0530

    add couple of __str__ defs

--- watch_sdk/models.py
+++ watch_sdk/models.py
@@ -22,9 +22,15 @@ class Platform(BaseModel):
     # google fit, apple healthkit, strava etc.
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class DataType(BaseModel):
     name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class EnabledPlatform(BaseModel):
