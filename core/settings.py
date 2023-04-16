ce7ba3a9f3999ee9ecaff95842b4ba284c6a023c
Pulkit Goyal <7895pulkit@gmail.com>
2023-04-16 17:00:10 +0530

    lower down traces sample rate for sentry

--- core/settings.py
+++ core/settings.py
@@ -240,5 +240,5 @@ else:
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production,
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.001,
     )
